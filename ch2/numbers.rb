5f962c2f2627056f0f4b1c833cb2e32c57f74b39
Abdihakim Warsame <amohwarsame@gmail.com>
2020-11-04 19:16:16 +0000

    MY Solution to the exercises at the end of chapter 2

---
+++ ch2/numbers.rb
@@ -0,0 +1,11 @@
+# How many Hours in a year?
+puts 365 * 24
+ 
+# How many minutes in a decade?
+puts (60 * 24 * 30 * 12) * 10
+
+# My age in seconds.
+puts ( 60 * 60 * 24 * 30 * 12) * 23
+
+# Convert author's age from seconds to years.
+puts 1111000000 / 60 / 60 / 24 / 30 / 12
