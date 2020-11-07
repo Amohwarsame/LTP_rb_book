00a5affa65a318487c4535283ce800b33f60bd93
Abdihakim Warsame <amohwarsame@gmail.com>
2020-11-07 23:58:14 +0000

    Edited the comments in the program

--- ch6/angry_boss.rb
+++ ch6/angry_boss.rb
@@ -1,4 +1,4 @@
-# The program(boss) asks you what do you want, then yells back what you tell him and proceeds to fire you.
+# The program(boss) asks you what do you want, then yells back what you tell it and proceeds to fire you.
 puts 'WHAT DO YOU WANT?!'
 answer = gets.chomp
 puts 'WHADDAYA MEAN ' + '"' + answer.upcase + '"' + '?!? ' + 'YOU\'RE FIRED!!'
